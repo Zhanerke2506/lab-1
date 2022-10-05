1b6559bfa6fb794b951ad135e47905bcdbbbf918
zhanerkeakimzhan <74965537+Zhanerke2506@users.noreply.github.com>
2022-10-05 22:21:01 +0600

    lab-4

--- lab-4/task-3.py
+++ lab-4/task-3.py
@@ -9,12 +9,24 @@
 # В восьмой строке выведите все символы строки через один в обратном порядке, начиная с последнего.
 # В девятой строке выведите длину данной строки.
 s = input("text:")
-print(s[2])
-print(s[-2])
-print(s[:5])
-print(s[:-2])
-print(s[::2])
-print(s[1::2])
-print(s[::-1])
-print(s[::-2])
-print(len(s))
+# print(s[2])
+# print(s[-2])
+# print(s[:5])
+# print(s[:-2])
+# print(s[::2])
+# print(s[1::2])
+# print(s[::-1])
+# print(s[::-2])
+
+print(len(s)) #длинасы
+print (s.islower()) #lower ma tekseredi
+print (s.isupper()) #upper ma tekseredi
+print (s.isdecimal())  #ondyk pa tekseredi
+print ('2'.isdigit()) #san ba tekseresi
+print (s.isnumeric()) #ishinde san bolsa true kaitarady
+print ('python3'.isalpha()) #aripter ma tekseredi
+print (s.rjust(10,'-')) #on zhagyna symbol kosady
+print (s.ljust(10,'-')) #sol zhagyna symbol kosady
+print (s.center(20,'*')) #textti center alyp symbol kosady
+print (s.find("a")) #isdeu
+print (s.replace('a',"A")) #a-ny A-ga auystyrady
